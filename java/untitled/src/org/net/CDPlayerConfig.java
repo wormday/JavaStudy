ce96654374c561fe9cac0bc605eb2f26c138b60e
wormday <sunwuqiang@gmail.com>
2016-12-04 07:52:22 +0800

    jsp

---
+++ java/untitled/src/org/net/CDPlayerConfig.java
@@ -0,0 +1,12 @@
+package net;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by sunwq on 2016/12/3.
+ */
+@ComponentScan
+@Configuration
+public class CDPlayerConfig {
+}
